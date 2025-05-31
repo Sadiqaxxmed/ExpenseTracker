d4be2c53965f27de5ef0b77488064ce09aec9f60
sadiqaxxmed <sadiqaxxmed@gmail.com>
2025-05-31 10:52:54 -0700

    DB setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,40 @@
 import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+    try {
+        await sql`CREATE TABlE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            catergory VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+
+
+            
+    )`;
+
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.log("Error intializing DB", error);
+        process.exit(1); // status code 1 means failure, 0 success 
+    }
+}
 
 app.get("/", (req, res) => {
     res.send("it's working");
 })
 
-app.listen(5001, () => {
-    console.log("Server is up and running on PORT:5001");
-})
-
+initDB().then(() => {
+    app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+    });
+});
