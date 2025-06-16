6b222008bb75bf0330a15ba2131041711957313d
sadiqaxxmed <sadiqaxxmed@gmail.com>
2025-06-16 07:21:58 -0700

    React native basic setup & styles

---
+++ mobile/app/index.jsx
@@ -0,0 +1,10 @@
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>      
+    </View>
+  );
+}
